61425c71f8762c9e30636022090289eb2fac7033
yuan <1909895443@qq.com>
2020-10-06 17:14:48 +0800

    增加web页面

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
-#!/usr/bin/php
 <?php
 
-//error_reporting(0);
+error_reporting(0);
 date_default_timezone_set("Etc/GMT-8");
 //获取加秒数的时间戳
 function getMillisecond() {
@@ -67,18 +66,97 @@ function start($phone, $password, $setStep) {
 	}
 }
 
-$time=date('y-m-d h:i:s',time());
-file_put_contents('info.log',$time."\n\n",FILE_APPEND);
-$file = file_get_contents("account.json");
-$account = json_decode($file);
-foreach($account as $user) {
-	$phone = $user->phone;
-	$password = $user->password;
-	$step = $user->step;
-	
-	$result = start($phone, $password, $step);
-	file_put_contents('info.log',"\t".$result."\n\n",FILE_APPEND);
+if(php_sapi_name() == 'cli') { //判断是否已脚本方式运行
+	$time=date('y-m-d h:i:s',time());
+	file_put_contents('info.log',$time."\n\n",FILE_APPEND);
+	$file = file_get_contents("account.json");
+	$account = json_decode($file);
+	foreach($account as $user) {
+		$phone = $user->phone;
+		$password = $user->password;
+		$step = $user->step + mt_rand(0, 10000);
 
-	sleep(1);
-}
-file_put_contents('info.log',"\n\n",FILE_APPEND);
+		$result = start($phone, $password, $step);
+		file_put_contents('info.log',"\t".$result."\n\n",FILE_APPEND);
+
+		sleep(1);
+	}
+	file_put_contents('info.log',"\n\n",FILE_APPEND);
+	exit;
+} else if(isset($_REQUEST['flag'])) {
+
+	$flag = $_REQUEST['flag'];
+	$phone = $_REQUEST['phone'];
+	$password = $_REQUEST['pwd'];
+	$step = $_REQUEST['step'];
+
+	if($flag == '1') {
+		$sevpwd=$_REQUEST['sevpwd'];
+		if($sevpwd != 'yuan'){
+			echo "{\"code\":\"404\",\"msg\":\"服务器密码错误\"}";
+			exit;
+		}
+		$file = file_get_contents("account.json");
+		$data = json_decode($file, true);
+		$data[$phone] = ['phone'=>$phone, 'password'=>$password, 'step'=>$step];
+		$account = json_encode($data);
+
+		$result=file_put_contents('account.json',$account);
+
+		$text = $result == False ? "{\"code\":\"405\",\"msg\":\"文件写入失败\"}" : "{\"code\":\"508\",\"msg\":\"账号添加成功\"}";
+		echo $text;
+
+	} else {
+		$result = start($phone, $password, $step);
+		echo $result;
+	}
+
+	exit;
+ }
+?>
+
+<!doctype html>
+<html lang="zh">
+<head>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>乐心刷步</title>
+	<link rel="stylesheet" type="text/css" href="css/styles.css">
+	<!--弹窗样式-->
+	<link rel="stylesheet" type="text/css" href="css/xtiper.css">
+</head>
+<body>
+<div class="htmleaf-container">
+	<div class="wrapper">
+		<div class="container">
+			<h1>Welcome</h1>
+			
+			<form class="form" action="./" method="post" id='port_form'>
+				<input type="text" placeholder="Phone" name="phone" id="phone">
+				<input type="password" placeholder="Password" name="pwd" id="pwd">
+				<input type="text" placeholder="step" name="step" id="step">
+				<input type="hidden" name="flag" id='flag' value='0' id="flag">
+				<button type='button' id="mul-button" style="width:130px">手动</button>
+				<button type='button' id="auto-button" style="width:130px">自动</button>
+			</form>
+		</div>
+		<ul class="bg-bubbles">
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+		</ul>
+	</div>
+</div>
+	<script src="https://cdn.bootcss.com/jquery/1.11.3/jquery.min.js"></script> 
+	<script src="js/xtiper.min.js" type="text/javascript"></script>
+	<script src="js/login.js"></script>
+</body>
+</html>
---
+++ js/login.js
@@ -0,0 +1,72 @@
+var xiha={
+	postData: function(url, parameter, callback, dataType, ajaxType) {
+		if(!dataType) dataType='json';
+		$.ajax({
+			type: "POST",
+			url: url,
+			async: true,
+			dataType: dataType,
+			json: "callback",
+			data: parameter,
+			success: function(data) {
+				if (callback == null) {
+					return;
+				} 
+				callback(data);
+			},
+			error: function(error) {
+				xtip.closeAll();
+				xtip.alert('网络错误',"e");
+			}
+		});
+	}
+}
+function trim(str){ //去掉头尾空格
+	return str.replace(/(^\s*)|(\s*$)/g, "");
+}
+
+function init(){
+	$('#mul-button').click(function(){
+		login(0);
+	});
+	$('#auto-button').click(function(){
+		login(1);
+	});
+}
+
+function isEmpty(str){
+	return str.length == 0;
+}
+
+function login(flag){
+
+	var phone=trim($('#phone').val()),
+		pwd=trim($('#pwd').val()),
+		step=trim($('#step').val());
+	xtip.closeAll();
+	if(isEmpty(phone)||isEmpty(pwd)||isEmpty(step)){
+		xtip.alert("不能留空，请重新输入！", "e");
+		return ;
+	}
+	if(flag == 0) {
+		var url = "?flag=0";
+	} else if(flag == 1) {
+		var sevpwd=window.prompt('此功能需要提供密码，联系站长获取：', '密码');
+		var url = "?flag=1&sevpwd="+sevpwd;
+	}
+	xiha.postData(url,"phone="+phone+"&pwd="+pwd+"&step="+step, function(d) {
+		if(d.code ==200){
+			var stepMax = Math.max.apply(Number,d.step.split(",").map(Number));
+			xtip.alert('刷步成功<br>当前最大步数：' + stepMax, "s");
+		} else if(d.code == 508){
+			xtip.alert(d.msg, "s");
+		}else{
+			xtip.alert(d.code+'<br>'+d.msg, "e");
+
+		}
+	});
+}
+	
+$(document).ready(function(){
+	init();
+});
